543cbc257f92db235346a8e61bcf310cc4de9f08
bishald0013 <bishal986473@gmail.com>
2023-11-11 11:34:09 +0530

    alert card added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,16 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Dashboard.css'
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import Alert from '../../alert/Alert';
+import { useCreateAlertMutation } from '../../services/alertAuthApi';
+import { useGetAlertsQuery } from '../../services/alertAuthApi';
 
 function Dashboard() {
 
   const alertType = ['Driving Licence', 'Fitness Expiry', 'RC Expiry', 'Pucc expiry Reminder']
-  const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedDate, setSelectedDate] = useState(null)
   const [alertData, setAlertData] = useState(null)
+  const [createAlert] = useCreateAlertMutation()
 
-  const handleSubmit = (event) => {
+  const [allAlerts, setAllAlerts] = useState({})
+
+  const token = localStorage.getItem('token');
+
+  const {data, isSuccess} = useGetAlertsQuery(token);
+
+  useEffect(() => {
+    if(isSuccess){
+      setAllAlerts(data);
+    }
+  }, [data, isSuccess])
+
+
+  
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
     const formData = new FormData(event.currentTarget);
@@ -18,27 +35,32 @@ function Dashboard() {
     const alertData = {
       alertName: formData.get('alertName'),
       alertType: formData.get('alertType'),
+      vehicleNumber: formData.get('vehicleNumber'),
       alertDate: selectedDate,
     }
 
     if(!alertData.alertName) throw new Error('Alert name is required');
     if(!alertData.alertType) throw new Error('Alert type is required');
     if(!alertData.alertDate) throw new Error('Alert date is required');
+    if(!alertData.vehicleNumber) throw new Error('Vehicle number is required');
 
     setAlertData(alertData);
 
-  }
+    const response = await createAlert({alertData, token});
 
-  console.log(alertData)
+  }
 
   return (
     <>
      <div className='main_container'>
         <div className='row my-5'>
-          <div className='col-lg-6 my-5'>
-            <span className='me-3'>Your Alerts</span>
-            {alertData === null ? <></> : <Alert alertData={alertData}/>}
+        <div className='col-lg-6 my-5'>
+          <span className='me-3'>Your Alerts</span>
+          <div className="row row-cols-1 row-cols-lg-2">
+            <Alert allAlert={allAlerts} />
           </div>
+        </div>
+
             <form className='col-lg-6 my-5' onSubmit={handleSubmit}>
               <div className=''>
                   <span className='me-3'>Create Alert</span>
@@ -47,7 +69,7 @@ function Dashboard() {
                         <input type="text" className="form-control" placeholder="Alert name" name='alertName' aria-label="Alert name" />
                     </div>
                     <div className="col  my-3">
-                        <input type="text" className="form-control" placeholder="Vehicle number" name='vehicleName' aria-label="Vehicle name" />
+                        <input type="text" className="form-control" placeholder="Vehicle number" name='vehicleNumber' aria-label="Vehicle name" />
                     </div>
                     <div className="col  my-3">
                       <select id="" name='alertType' className="form-select">
