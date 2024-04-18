123a573849b9e6e3c8edd39602fb409341cfdea5
Biptu <62784362+biptu123@users.noreply.github.com>
2024-04-18 21:51:08 +0530

    Update Footer.js: changed email

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -13,7 +13,7 @@ const Footer = () => {
           <p className="mb-0">&copy; 2024 relynrelax. All rights reserved.</p>
 
           <p className="mb-0">
-            Contact us: hirakmittu@gmail.com | Phone: +91 9395783957
+            Contact us: helpdesk.relynrelax@gmail.com | Phone: +91 9395783957
           </p>
         </div>
       </div>
