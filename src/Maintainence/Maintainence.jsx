d024d9b2bd59d336fc933b295dfbefcaa4e8a6a6
Bishal Deb <102280193+bishald0013@users.noreply.github.com>
2024-06-12 10:54:25 +0530

    maintainence break

---
+++ src/Maintainence/Maintainence.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Break from '../images/break.jpg'
+
+function Maintainence() {
+  return (
+    <div className='container w-50'>
+        <img src={Break} class="img-fluid" alt="..." />
+    </div>
+  )
+}
+
+export default Maintainence
