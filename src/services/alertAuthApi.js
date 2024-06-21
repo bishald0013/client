185d7218bc218b69775a50e6a54176f470e73c39
Biptu Das <c.biptu.pc.mail@gmail.com>
2024-06-21 20:58:59 +0530

    logo changed

--- src/services/alertAuthApi.js
+++ src/services/alertAuthApi.js
@@ -4,8 +4,8 @@ export const alertAuthApi = createApi({
   reducerPath: "alertAuthApi",
 
   baseQuery: fetchBaseQuery({
-    // baseUrl: "https://server-side-v65f.onrender.com/api/alert",
-    baseUrl: "http://localhost:5001/api/alert",
+    baseUrl: "https://server-side-v65f.onrender.com/api/alert",
+    // baseUrl: "http://localhost:5001/api/alert",
   }),
 
   endpoints: (builder) => ({
